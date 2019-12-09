f3c014e5cf2e1e292ff5e980a978e04acde4af5e
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2019-12-09 19:08:52 +0100

    initial commit

---
+++ PauseGame/Assets/PauseGame/Scripts/B_PauseResume.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_PauseResume : MonoBehaviour
+{
+    [SerializeField] bool IsPaused = false;
+    public KeyCode pauseKey;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(pauseKey))
+        {
+            IsPaused = !IsPaused;
+
+            if (IsPaused)
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void ButtonResume()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+    }
+}
---
+++ PauseGame/Assets/PauseGame/Scripts/PauseGame02.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame02 : MonoBehaviour
+{
+    [SerializeField] bool IsPaused = false;
+    public KeyCode pauseKey;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GamePause();
+    }
+    void GamePause()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            IsPaused = !IsPaused;
+
+            if (IsPaused)
+            {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+}
